3c724178054f6409650c642532e08871990031ea
User <you@example.com>
2022-11-09 20:24:44 +0200

    LambdaExpression and LoremImpsum homework start

--- src/main/java/org/sda/homeworks/LambdaExpression.java
+++ src/main/java/org/sda/homeworks/LambdaExpression.java
@@ -1,4 +1,11 @@
 package org.sda.homeworks;
 
+/**
+ * Addition, subtraction, multiplication, division
+ * The sum of elemtns(int type) of the list
+ * Number of words in the unput expression (list containing elemnts of type String)
+ * List before and after sorting (use the Arrays class and lambda epressions: String:: compareToIgnoreCase as a Comparator)
+ */
 public class LambdaExpression {
+
 }
---
+++ src/main/java/org/sda/homeworks/LoremImpsum.java
@@ -0,0 +1,18 @@
+package org.sda.homeworks;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Create a file with a "Lorem ipsum" paragraph within -
+ * it can be done by copy-pasting existing paragraph or generating it dynamically using Java library. Read that file.
+ *
+ * a) Count words.
+ * b) Count special signs(like comma, dot, spaces).
+ * c) Select one word and print it's number of occurences.
+ *
+ * @author Sander
+ */
+
